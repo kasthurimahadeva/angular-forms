bfd0c1c5217c9b5f8105a4b9577f8b44f421f0fd
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-06-01 01:07:37 +0530

    post form data to dummy server

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -19,13 +20,21 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings: UserSettings = {...this.originalUserSettings};
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
     console.log(form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success: ', result),
+      error => console.log('error: ', error)
+    );
+  }
+
+  onBlur(field: NgModel) {
+    console.log('On blur', field.valid);
   }
 
 }
